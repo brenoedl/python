5c1d43d628f44f07dddfaa1738c344ba2e712a96
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-03-28 10:14:08 -0300

    4m

--- exercicios/ex005/ex005.py
+++ exercicios/ex005/ex005.py
@@ -7,7 +7,7 @@ d = n1/n2
 di = n1//n2
 rd = n1%n2
 e = n1**n2
-print('A soma é {} e a sobtração é {}'.format(s,sb))
-print('O produto é {} e a divisão é {}'.format(m,d))
-print('A divisão inteira é {} e oresto da divisão é {}'.format(di,rd))
+print('A soma é {} e a sobtração é {}'.format(s,sb),end=' ')
+print('O produto é {} e a divisão é {:.3f}'.format(m,d),end=' ')
+print('A divisão inteira é {} e oresto da divisão é {}'.format(di,rd),end=' ')
 print('A exponenciação é {}'.format(e))
