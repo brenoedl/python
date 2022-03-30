00c3fd9c3326300e1d3f7937f3c907dba6942637
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-03-29 22:08:44 -0300

    nghg

---
+++ desafios/des003/des003_p04.py
@@ -0,0 +1,8 @@
+m = float(input('Uma distancia em metros: '))
+Km = m /1000
+Hm = m /100
+Dm = m /10
+dm = m *10
+cm = m *100
+mm = m *1000
+print('A medida {}m corresponde a \n {}Km \n {}hm \n {}dam \n {:.0f}dm \n {:.0f}cm \n {:.0f}mm.'.format(m,Km,Hm,Dm,dm,cm,mm))
