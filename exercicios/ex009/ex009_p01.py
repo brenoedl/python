0c1e513ad592ad566df517fcf77bce7dfabd8b14
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-04-26 15:47:55 -0300

    exersicio da aula 010 completo

---
+++ exercicios/ex009/ex009_p01.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite o seu nome: '))
+if nome == 'Breno':
+    print('Que nome lindo você tem!')
+else:
+    print('Que nome comun você tem!')
+print('Bom dia, {}!'.format(nome))
