767851ffee218da77d27c483d1454a9269cd024c
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-05-18 18:36:29 -0300

    desafio 007 parte 08 completo

---
+++ desafios/des007/des007_p08.py
@@ -0,0 +1,14 @@
+peso = float(input('Qual o seu peso? (Kg) '))
+altura = float(input('Qual a sua altura? (m) '))
+imc = peso / (altura ** 2)
+print('O IMC dessa pessoa é de {:.1f}'.format(imc))
+if imc < 18.5:
+    print('Você está abaixo do peso normal')
+elif 18.5 <= imc < 25:
+    print('PARABENS! Você está na faixa de peso normal')
+elif 25 <= imc < 30:
+    print('Você está em SOBREPESO')
+elif 30 <= imc < 40:
+    print('Você está em OBESIDADE CUIDADO!')
+elif imc >= 40:
+    print('Você está em OBESIDADE MORBIDA EXTREEMO CUIDADO!')
