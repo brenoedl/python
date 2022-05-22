cad68bca62c6e5cc2e603cadbcbeab75fc7b290f
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-05-22 12:37:34 -0300

    desafio 007 parte 10 completo

---
+++ desafios/des007/des007_p10.py
@@ -0,0 +1,45 @@
+from random import randint
+from time import sleep
+itens = ('Pédra' , 'Papel' , 'Tesoura')
+computador = randint(0 , 2)
+print('''Suas opçõs
+[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura''')
+jogador = int(input('Qual é a sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!')
+print('+=' * 15)
+print('O computador jogou {}'.format(itens[computador]))
+print('O  jogaddor jogou {}'.format(itens[jogador]))
+print('+=' * 15)
+if computador == 0:
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('JOGADOR VENCE')
+    elif jogador == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('Jogada invalida')
+elif computador == 1:
+    if jogador == 0:
+        print('COMPUTADOR VENCE')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('JOGADOR VENCE')
+    else:
+        print('Jogada invalida')
+elif computador == 2:
+    if jogador == 0:
+        print('JOGADOR VENCE')
+    elif jogador == 1:
+        print('COMPUTADOR VENCE')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('Jogada invalida')
