7099958d1c1990b9cea6843d04848dd525478cf1
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-06-11 22:51:02 -0300

    desafio 010 parte 03 completo

---
+++ desafios/des010/des010_p03.py
@@ -0,0 +1,27 @@
+from random import randint
+v = 0
+while True:
+    jogador = int(input('Digite um valor: '))
+    computador = randint(0, 10)
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('par ou impar? [P/I]')).strip().upper()[0]
+    print(f'Você jogou {jogador} e o computador {computador}. total de  {total}.', end=' ')
+    print('DEU PAR' if total % 2 == 0 else 'DEU IMPAR')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você venceu!')
+            v += 1
+        else:
+            print('Você perdeu!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você venceu!')
+            v += 1
+        else:
+            print('Você perdeu!')
+            break
+    print('Vamos jogar novamente')
+print(f'GEME OVER você venceu {v} vezes.')
