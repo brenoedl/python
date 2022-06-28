418d3f3b57afbae3e824476119bc30de6afbb785
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-06-27 23:58:39 -0300

    desafio 011 parte 03  completo

---
+++ desafios/des011/des011_p03.py
@@ -0,0 +1,5 @@
+from random import randint
+valores = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10))
+print(f'Eu sorteei os valores {valores}')
+print(f'O maior valor sorteado {max(valores)}')
+print(f'O menor valor sorteado {min(valores)}')
