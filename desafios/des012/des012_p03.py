b055e11532c161b9f84f3a7b344ab8b8cd15db40
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-07-05 16:36:53 -0300

    desafio 012 parte 03 completo

---
+++ desafios/des012/des012_p03.py
@@ -0,0 +1,13 @@
+listanum = list()
+for c in range(0, 5):
+    n = int(input('Digite um número: '))
+    if c == 0 or n > listanum[-1]:
+        listanum.append(n)
+    else:
+        pos = 0
+        while pos < len(listanum):
+            if n <= listanum[pos]:
+                listanum.insert(pos, n)
+                break
+            pos += 1
+print(f'Os valores digitados em ordem foram {listanum}')
