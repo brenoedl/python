ee7b928fe6b06dc09ef216316dbef3d346de01bd
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-07-21 20:31:41 -0300

    exercício 018 e pasta do desafio 014 feitos

---
+++ exercicios/ex018/ex018.py
@@ -0,0 +1,8 @@
+dados = {'nome': 'Breno', 'idade': 20}
+dados['sexo'] = 'M'
+for k, v in dados.items():
+    print(f'{k} = {v}')
+print(dados)
+print(dados.keys())
+print(dados.values())
+print(dados.items())
