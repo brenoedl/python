42994dde1fc2cb0113cfd6d1a67704b0f96e7033
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-07-28 14:15:13 -0300

    Update des014_p01.py

--- desafios/des014/des014_p01.py
+++ desafios/des014/des014_p01.py
@@ -4,7 +4,7 @@ aluno['media'] = float(input(f'Media do {aluno["nome"]}:'))
 if aluno['media'] >= 7:
     aluno['situação'] = 'Aprovado'
 elif 5 <= aluno['media'] < 7:
-    aluno['situação'] = 'Recuperação'
+    aluno['situação']= 'Recuperação'
 else:
     aluno['situação'] = 'Reprovado'
 print('-=' * 20)
