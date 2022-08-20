11957fc5f79cb27e981b4c4a5f8bc6a3d7b67aac
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-08-19 21:07:14 -0300

    desafio 015 parte 02 completo

---
+++ desafios/des015/des015_p02.py
@@ -0,0 +1,10 @@
+def escreva(msg):
+    tan = len(msg) + 4
+    print('~' * tan)
+    print(f'  {msg}')
+    print('~' * tan)
+
+
+escreva('Breno Emanuel')
+escreva('Curso em video de python')
+escreva('YouTube')
