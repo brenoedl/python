414c75f5c3feba1fc7602eacb58ec794b385d0cb
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-09-29 20:59:45 -0300

    exercício 021 feito

---
+++ exercicios/ex021/ex021.py
@@ -0,0 +1,9 @@
+import uteis
+n = int(input('Digite um número: '))
+f = uteis.fatorial(n)
+d = uteis.dobro(n)
+t = uteis.triplo(n)
+print(f'Você digitou o número {n}')
+print(f'O dobro é {d}')
+print(f'O triplo é {t}')
+print(f'O fatorial é {f}')
---
+++ exercicios/ex021/uteis.py
@@ -0,0 +1,15 @@
+def fatorial(num):
+    f = 1
+    for c in range(1, num+1):
+        f *= c
+    return f
+
+
+def dobro(numd):
+    d = numd * 2
+    return d
+
+
+def triplo(numt):
+    t = numt * 3
+    return t
