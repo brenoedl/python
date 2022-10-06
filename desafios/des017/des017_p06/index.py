c3955648cce1e6d132e1d74d7b426808ec71274a
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-10-06 14:21:28 -0300

    desafio 017 parte 06 completo

---
+++ desafios/des017/des017_p06/index.py
@@ -0,0 +1,5 @@
+from desafios.des017.des017_p06.utilidadescev import moeda
+from desafios.des017.des017_p06.utilidadescev import dados
+
+p = dados.leiadinheiro('dDigite um preço: R$')
+moeda.resumo(p, 10, 13)
---
+++ desafios/des017/des017_p06/utilidadescev/__init__.py
@@ -0,0 +1,1 @@
+from desafios.des017.des017_p06.utilidadescev import dados, moeda
